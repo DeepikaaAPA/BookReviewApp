18de5a3a56f469989b61bb02247c55719ac8dab6
DeepikaaAPA <deepikaarjun@gmail.com>
2024-08-25 15:06:12 +0530

    adding auth route register

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const userRouter = require("./routes/userRouter");
+const authRouter = require("./routes/authRouter");
 const app = express();
+
+app.use("/api/v1/auth", authRouter);
 
 module.exports = app;
---
+++ controllers/userController.js
@@ -0,0 +1,7 @@
+const userController = {
+  createUser: (req, res) => {
+    console.log("hello");
+    res.send("helo");
+  },
+};
+module.exports = userController;
